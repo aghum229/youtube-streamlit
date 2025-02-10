3bc6e6f61c31b3f7a5a2e6c88e1f9d8303b621ee
aghum229 <m229@aitech-inc.co.jp>
2025-02-11 08:41:36 +0900

    Add files via upload

--- Pythontest.py
+++ Pythontest.py
@@ -11,7 +11,14 @@ bar = st.progress(0)
 for i in range(100):
 	latest_iteration.text(f'Iteration{i+1}')
 	bar.progress(i+1)
-	time.sleep(0.1)
+	time.sleep(0.05)
 
 'Done!!!'
 
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+	right_column.write('右カラムです。')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
