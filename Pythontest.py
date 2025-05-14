1201955ec089d2dfdf703c58fb91c667b3abd802
aghum229 <m229@aitech-inc.co.jp>
2025-05-14 20:08:54 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -7,8 +7,8 @@ button_css = f"""
     font-weight  : bold                ;/* 文字：太字                   */
     color        : #000                ;
     border       :  1px solid #000     ;/* 枠線：ピンク色で5ピクセルの実線 */
-    border-radius: 10px 10px 10px 10px ;/* 枠線：半径10ピクセルの角丸     */
-    background   : #0FF               ;/* 背景色：aqua            */
+    border-radius: 5px 5px 5px 5px     ;/* 枠線：半径10ピクセルの角丸     */
+    background   : #0FF                ;/* 背景色：aqua            */
   }}
 </style>
 """
