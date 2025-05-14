a5c5710358e19420db43cc67a840ee1f4fd76b1e
aghum229 <m229@aitech-inc.co.jp>
2025-05-14 17:20:30 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -1,38 +1,6 @@
 import streamlit as st
 import time
 
-# サイドバーにテーマの選択オプションを追加
-theme = st.sidebar.radio(
-    "テーマを選択してください",
-    options=["ライトモード", "ダークモード"]
-)
- 
-# 選択されたテーマに基づいてスタイルを適用
-if theme == "ライトモード":
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #FFFFFF;
-            color: #000000;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-else:
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #2c3e50;
-            color: #ecf0f1;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
 st.title('文書・記録管理　メニュー')
 
 # st.write('<span style="color:red;background:pink">該当するデータがありません・・・・</span>',
