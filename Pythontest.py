faf842220a408a93697c61ac870350ac98de6a51
aghum229 <m229@aitech-inc.co.jp>
2025-05-16 00:06:04 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -14,8 +14,8 @@ div.stButton > button {
     color        : #000;
     border-radius: 5px 5px 5px 5px     ;/* 枠線：半径10ピクセルの角丸     */
     background   : #0FF                ;/* 背景色：aqua            */
-    width: 200px; /* ボタンの横幅を固定値に設定 */
-    max-width: 200px; /* 必要に応じて最大幅も設定 */
+    width: 250px; /* ボタンの横幅を固定値に設定 */
+    max-width: 250px; /* 必要に応じて最大幅も設定 */
     height: 50px;
 }
 </style>
