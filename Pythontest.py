2829e38732144d65e7e33d8ee726322dbb104379
aghum229 <m229@aitech-inc.co.jp>
2025-05-28 07:23:27 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -109,7 +109,7 @@ def display_footer():
 
 def show_main_screen():
     display_mainheader()
-    with st.container(border=True):
+    # with st.container(border=True):
         st.markdown(button_style, unsafe_allow_html=True)
         button1 = st.button("1.製造関連", on_click=set_screen, args=('other1',))
         button2 = st.button("2.ＩＳＯ関連", on_click=set_screen, args=('other2',))
