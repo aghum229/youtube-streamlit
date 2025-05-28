6e779e82c87cf5b1106a3cb7469a86190b9029d1
aghum229 <m229@aitech-inc.co.jp>
2025-05-28 14:02:22 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -330,7 +330,7 @@ def show_other1_screen():
         _, right = st.columns([0.5, 0.5])
         with right:
             with st_opaque_container(border=True):
-                st.markdown("""
+                btn0 = st.markdown("""
                     <style> \
                     .custom-button { \
                        background-color: #FF0; \
@@ -349,6 +349,7 @@ def show_other1_screen():
                     </style> \
                     <button class="custom-button">⏎☆メイン画面☆　へ戻る</button>
                 """, unsafe_allow_html=True)
+                btn0.on_click(set_screen, args=('main',))
                 # btn0 = st.button("⏎☆メイン画面☆　へ戻る", use_container_width=True, on_click=set_screen, args=('main',))
                 # btn1 = st.button("⏎1.製造関連メニュー　へ戻る", use_container_width=True, on_click=set_screen, args=('other1',))
     st.markdown(write_css1, unsafe_allow_html=True)
