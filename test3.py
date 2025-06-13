59a89d97dd784679ae15d27ea9e4eba8eba595c7
aghum229 <m229@aitech-inc.co.jp>
2025-06-13 09:16:47 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,40 @@
+import streamlit as st
+import requests
 
+# Streamlitのシークレットから値を取得
+client_id = st.secrets["client_id"]
+client_secret = st.secrets["client_secret"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+token_url = st.secrets["token_url"]
+
+# OAuthリクエストの送信
+payload = {
+    "grant_type": "password",
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "username": username,
+    "password": password
+}
+
+
+try:
+    response = requests.post(token_url, data=payload)
+    response.raise_for_status()
+
+    auth_response = response.json()
+    access_token = auth_response.get("access_token")
+    instance_url = auth_response.get("instance_url")
+
+    if access_token:
+        st.write("✅ **成功しました！**")
+        st.write(f"🔑 Access Token: `{access_token[:40]}...`")
+        st.write(f"🌍 Instance URL: `{instance_url}`")
+    else:
+        st.write("❌ **トークンが受信されませんでした。**")
+        st.write(auth_response)
+    
+    
+except requests.exceptions.RequestException as e:
+    print("❌ Salesforce への接続エラー：")
+    print(e)
