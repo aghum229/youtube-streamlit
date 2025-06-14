6ed810c47b5c287a246a7eb99cc7b7b83e597dcf
aghum229 <m229@aitech-inc.co.jp>
2025-06-14 18:49:16 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -331,12 +331,11 @@ def display_footer():
 def display_container(color, text):
     with st_fixed_container(mode="fixed", position="top", transparent=True):
         st.markdown(
-            f"<p style='text-align:center; width:100%; margin: 0;'> \
-            <span style='display: block; margin: auto; font-size: 40px; font-weight: bold; color: {color}; line-height: 0.5;'>{text}</span> \
-            </p>",
+            f"<div style='display: flex; justify-content: center; align-items: center; width: 100%;'> \
+            <span style='font-size: 40px; font-weight: bold; color: {color};'>{text}</span> \
+            </div>",
             unsafe_allow_html=True
         )
-
 
 _= '''
 def display_container(color, text):
