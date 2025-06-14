8724819f73c400e8e76c9279a1f1298b6f305f8c
aghum229 <m229@aitech-inc.co.jp>
2025-06-14 09:33:50 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -43,13 +43,13 @@ secrets = carregar_credenciais()
 
 # Função de autenticação do Salesforce usando as credenciais do secrets
 def authenticate_salesforce():
-    auth_url = f"{secrets['DOMAIN']}/services/oauth2/token"
+    auth_url = st.secrets["token_url"]
     auth_data = {
         'grant_type': 'password',
-        'client_id': secrets['CLIENT_ID'],
-        'client_secret': secrets['CLIENT_SECRET'],
-        'username': secrets['USERNAME'],
-        'password': secrets['PASSWORD']
+        'client_id': secrets['client_id'],
+        'client_secret': secrets['client_secret'],
+        'username': secrets['username'],
+        'password': secrets['password']
     }
     try:
         response = requests.post(auth_url, data=auth_data, timeout=10)
