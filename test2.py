dfeafe1fcba206f72267a8d60e5d072e05527aad
aghum229 <m229@aitech-inc.co.jp>
2025-06-16 00:35:45 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,9 +16,11 @@ if response.status_code == 200:
 else:
     st.write(f"Failed to fetch file: {response.status_code}")
 '''
+response = none
 response = requests.get(st.secrets["text_path"], headers={"Cache-Control": "no-cache"})
 # response = requests.get(st.secrets["text_path"], stream=True)
 if response.status_code == 200:
+    text_content = none
     text_content = "\n".join(line.decode("utf-8") for line in response.iter_lines())
     st.write(text_content)
 else:
