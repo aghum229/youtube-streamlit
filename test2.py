1f53e55ec8f2a5036bd149b786c1297037c98fa7
aghum229 <m229@aitech-inc.co.jp>
2025-06-15 12:25:19 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -57,7 +57,8 @@ for i in range(st.session_state.button_count):
     col1, col2 = st.columns(2)
     with col1:
         # button_name = st.text_input(f"ボタン {i+1} の名前:", key=f"button_name_{i}", value=f"ボタン{i+1}")
-        st.session_state.button_names.append(f"ボタン {i+1} の名前:", key=f"button_name_{i}")
+        # st.session_state.button_names.append(button_name)
+        st.button(f"ボタン{i+1}", key=f"dynamic_button_{i}")
     with col2:
         st.button(f"{button_name} を削除", on_click=remove_button, args=(i,), key=f"remove_button_{i}")
 
