46a35213fb50e2a82705cab2b39e8ed570496a99
aghum229 <m229@aitech-inc.co.jp>
2025-07-10 21:14:40 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -194,8 +194,7 @@ def encontrar_item_por_nome(sf, item_name):
     query = f"""
         SELECT Name, snps_um__ProcessOrderNo__c
         FROM snps_um__Process__c
-        WHERE Name[0:len(Name)-3] LIKE '%{item_name}%' AND snps_um__ProcessOrderNo__c = 999
-        LIMIT 1
+        WHERE Name LIKE '%{item_name}%' AND snps_um__ProcessOrderNo__c = 999
     """
     try:
         result = sf.query(query)
