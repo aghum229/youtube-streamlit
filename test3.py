d17c871d1c5221b6bdc36edecbfb18a4b9212e98
aghum229 <m229@aitech-inc.co.jp>
2025-07-10 21:31:11 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -80,7 +80,7 @@ def consultar_salesforce(production_order, sf):
                snps_um__ProdOrder__r.Name, snps_um__Status__c, snps_um__WorkPlace__r.Id, 
                snps_um__WorkPlace__r.Name, snps_um__StockPlace__r.Name, snps_um__Item__c, 
                snps_um__Process__r.AITC_Acumulated_Price__c, AITC_OrderQt__c, snps_um__EndDateTime__c, 
-               snps_um__Item__r.AITC_PrintItemName__c, AITC_JobInstSheetURL__c
+               snps_um__Item__r.AITC_PrintItemName__c
         FROM snps_um__WorkOrder__c 
         WHERE snps_um__ProdOrder__r.Name = '{production_order}'
     """
