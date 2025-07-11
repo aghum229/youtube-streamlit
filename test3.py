fc95760c6d02af42d152cc4c3b61386efadc1e64
aghum229 <m229@aitech-inc.co.jp>
2025-07-11 17:19:25 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -178,6 +178,8 @@ def encontrar_item_por_nome00(sf, item_name):
     query = f"""
         SELECT snps_um__ItemName__c
         FROM snps_um__Item__c
+        # WHERE snps_um__ItemName__c LIKE '%{item_name}%'
+        LIMIT 10
     """
     try:
         result = sf.query(query)
