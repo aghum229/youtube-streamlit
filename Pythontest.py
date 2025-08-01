8765bbb4faa5d298d2c6ac9b76b8628ce61fb76f
aghum229 <m229@aitech-inc.co.jp>
2025-08-02 06:28:30 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -384,7 +384,7 @@ def show_main_screen():
         st.image("aitech_logo_D.png", use_container_width=True)
     with right:
         st.write('(仮)')
-    # display_container('yellow', '☆メイン画面☆')
+    display_container('yellow', '☆メイン画面☆')
     display_line()
     st.markdown(button_style, unsafe_allow_html=True)
     button_set('button1', '1.製造関連', 'other1')
