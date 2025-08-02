3a07e08cb3d27bd227c7ead7d3272c6436684dc3
aghum229 <m229@aitech-inc.co.jp>
2025-08-02 15:43:19 +0900

    Update Pythontest.py

--- Pythontest.py
+++ Pythontest.py
@@ -399,6 +399,7 @@ def show_main_screen():
     display_line()
     st.markdown(button_style, unsafe_allow_html=True)
     button_set('button0', '0.ショートカット', 'other0')
+    st.write('  ')
     button_set('button1', '1.製造関連', 'other1')
     button_set('button2', '2.ＩＳＯ関連', 'other2')
     button_set('button3', '3.労務関連', 'other3')
