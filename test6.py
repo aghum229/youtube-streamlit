f1113fdaf5fa76992f19b69de2660b8e6fbef948
aghum229 <m229@aitech-inc.co.jp>
2025-08-04 22:12:31 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -249,8 +249,10 @@ if not st.session_state['owner']:
     st.session_state['owner'] = st.text_input("担当者コードを入力してください (3～4桁、例: 999:",
                                               max_chars=4,
                                               key="owner_input")
-    if not st.session_state['owner'] and len(st.session_state['owner']) <= 4 and st.session_state['owner'].isdigit():
+    if not st.session_state['owner'] and st.session_state['owner'].isdigit():
         st.stop()
+    else:
+        st.warning("入力を待っています...")
 
 _= '''
 # Controle de exibição: sucesso ou formulário
