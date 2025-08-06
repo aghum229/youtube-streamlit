5ee74aa0bccfd0b48ad9f4fc3a229a0ae4b3ae69
aghum229 <m229@aitech-inc.co.jp>
2025-08-06 09:56:12 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -531,7 +531,7 @@ else:
                         zkShoBu = zkIko
                         zkShoU = zkIko
                     else:
-                        
+                        none
                     _= '''
                     zkHin = record["zkHinban__c"].splitlines()   # zk品番
                     zkKan = record["zkKanryoKoutei__c"].splitlines()   # zk完了工程
