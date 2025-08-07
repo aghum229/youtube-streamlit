2cdda20e4ca81484a65e9b1cd99d661206e92ecb
aghum229 <m229@aitech-inc.co.jp>
2025-08-07 10:32:50 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -503,7 +503,7 @@ else:
             hinmei = st.text_input("品名:", key="hinmei", value="-")
     
         submit_button = st.form_submit_button("データベースに保存")
-        add_del_flag = 0
+        add_del_flag = 1
         if submit_button:
             _= '''
             item_name_input = st.session_state.manual_input_value.strip()
