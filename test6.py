38058ac5459de045ff7cf67d00738e65fb6da2d8
aghum229 <m229@aitech-inc.co.jp>
2025-08-08 08:19:34 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -3,12 +3,12 @@ import requests
 
 from streamlit_qrcode_scanner import qrcode_scanner
 import pandas as pd
-import os
+# import os
 import pytz
-from simple_salesforce import Salesforce
+# from simple_salesforce import Salesforce
 from datetime import datetime as dt
-import re
-import time
+# import re
+# import time
 import gspread
 from google.oauth2.service_account import Credentials
 from gspread_dataframe import set_with_dataframe
