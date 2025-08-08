3eb08ed53d88b48fe7c9aeb338f80c8084ff50bd
aghum229 <m229@aitech-inc.co.jp>
2025-08-09 01:21:55 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -468,14 +468,14 @@ else:
         height=0,
     )
     
-    _= '''
+    # _= '''
     submit_button_modify = st.button("再入力(移行票番号)")
     
     if submit_button_modify:
         st.write("ok")
-        # st.session_state.manual_input_value = ""
+        st.session_state.manual_input_value = ""
         # st.rerun()
-    '''
+    # '''
     
     _= '''
     # Exibir câmera apenas se production_order for None e show_camera for True
