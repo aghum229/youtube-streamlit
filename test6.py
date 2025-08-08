e931fd3d2bead63329e24a266eb0a9183fcadc33
aghum229 <m229@aitech-inc.co.jp>
2025-08-09 01:23:51 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -474,7 +474,7 @@ else:
     if submit_button_modify:
         st.write("ok")
         st.session_state.manual_input_value = ""
-        # st.rerun()
+        st.rerun()
     # '''
     
     _= '''
