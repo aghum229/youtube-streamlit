52731f7b2989d35e2d0e19cb1971a946d8c1aef5
aghum229 <m229@aitech-inc.co.jp>
2025-08-10 09:15:20 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -319,9 +319,9 @@ def reset_form():
 
 def styled_text(
     text,
-    bg_color="#f0f8ff",
-    text_color="#000000",
-    font_size="24px",
+    bg_color,
+    text_color,
+    font_size,
     height="26px",
     margin_top="5px",
     margin_bottom="5px"
@@ -332,7 +332,7 @@ def styled_text(
             background-color:{bg_color};
             padding:2px;
             border-radius:2px;
-            min-height:{height};
+            height:{height};
             margin-top:{margin_top};
             margin-bottom:{margin_bottom};
         ">
@@ -613,13 +613,13 @@ else:
         # st.write(f"棚番: {tanaban}")
         # styled_text(f"棚番 : {tanaban}", bg_color="#ffe4e1", text_color="#333333")
         with left:
-            styled_text(f"作業者 (社員番号) : ", bg_color="#ffe4e1", text_color="#333333")
-            styled_text(f"移行票番号 : ", bg_color="#ffe4e1", text_color="#333333")
-            styled_text(f"棚番 : ", bg_color="#ffe4e1", text_color="#333333")
+            styled_text(f"作業者 (社員番号) : ", bg_color="#ffe4e1", text_color="#333333", font_size="18px")
+            styled_text(f"移行票番号 : ", bg_color="#ffe4e1", text_color="#333333", font_size="18px")
+            styled_text(f"棚番 : ", bg_color="#ffe4e1", text_color="#333333", font_size="18px")
         with right:
-            styled_text(f"{owner_value}", bg_color="#ffe4e1", text_color="#333333")
-            styled_text(f"{production_order_value}", bg_color="#ffe4e1", text_color="#333333")
-            styled_text(f"{tanaban}", bg_color="#ffe4e1", text_color="#333333")
+            styled_text(f"{owner_value}", bg_color="#ffe4e1", text_color="#333333", font_size="24px")
+            styled_text(f"{production_order_value}", bg_color="#ffe4e1", text_color="#333333", font_size="24px")
+            styled_text(f"{tanaban}", bg_color="#ffe4e1", text_color="#333333", font_size="24px")
         if st.session_state.data:
             hinban = st.text_input("品番:", key="hinban", value=default_hinban)
             process_order = st.number_input("工程順序:", value=default_process_order, step=1, key="process_order")
