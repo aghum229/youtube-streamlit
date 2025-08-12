7edf059221956257ea32e56a8d800cb742554108
aghum229 <m229@aitech-inc.co.jp>
2025-08-12 12:22:17 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -539,8 +539,8 @@ else:
                     #st.session_state.show_camera = False
                     st.session_state.qr_code = None  # 処理済みなのでクリア
                     # st.rerun()
-                    st.session_state.trigger_rerun = True
-                if st.session_state.get("trigger_rerun"):
+                    # st.session_state.trigger_rerun = True
+                # if st.session_state.get("trigger_rerun"):
                     st.session_state.show_camera = False
                     st.session_state.trigger_rerun = False
                     # st.rerun()
