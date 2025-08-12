43188e6b1646b67d50a6a94e68629f58134a9d33
aghum229 <m229@aitech-inc.co.jp>
2025-08-13 01:38:41 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -567,12 +567,13 @@ else:
         # tanaban = st.text_input("棚番号を選択または入力してください (例: H-15):",
         #                     max_chars=6,
         #                     key="manual_input_tana")
+        st.write(tanaban_select != "完A-0")
         if tanaban_select != "" and tanaban_select != "完A-0": # 完A-0は存在しない置き場(変更前提の初期値としてのみ利用)
             # st.session_state.tanaban = tanaban_select
             st.session_state.tanaban_select_temp = tanaban_select
             st.session_state.show_camera = False
             st.session_state.qr_code_tana = True
-            st.write(st.session_state.qr_code_tana)
+            # st.write(st.session_state.qr_code_tana)
             st.rerun()  # 再描画して次のステップへ
     else:
         st.write(st.session_state.tanaban_select_temp)
