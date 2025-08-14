44ed8b1e58a501b5adf72ab6d3de0c6c1dbab983
aghum229 <m229@aitech-inc.co.jp>
2025-08-15 00:27:34 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -40,7 +40,7 @@ import numpy as np
 reader = easyocr.Reader(['ja', 'en'])
 
 # 画像ファイルのパス
-image_path = 'TanaMap20250814.png'
+image_path = 'TanaMap20250815.png'
 
 # OCR実行
 results = reader.readtext(image_path)
