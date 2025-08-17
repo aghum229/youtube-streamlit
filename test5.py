37e612174fda154dc91ca73961acdfb200d55b9a
aghum229 <m229@aitech-inc.co.jp>
2025-08-17 19:18:04 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -63,9 +63,9 @@ else:
             break
 
     if target_center:
-        radius_px = 26
+        radius_px = 30
         image_with_circle = image_np.copy()
-        cv2.circle(image_with_circle, target_center, radius_px, (255, 0, 0), thickness=3)
+        cv2.circle(image_with_circle, target_center, radius_px, (255, 0, 0), thickness=4)
 
         st.image(image_with_circle, caption=f"{target_text} の位置に赤い円（○）を描画", use_container_width=True)
         st.success(f"{target_text} を検出しました。座標: {target_center}")
