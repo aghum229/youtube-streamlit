0947557c0f88c57fd06b9c9b25d036deb1687f47
aghum229 <m229@aitech-inc.co.jp>
2025-08-18 19:03:26 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -85,7 +85,7 @@ else:
             results = reader.readtext(image_np)
             target_center = None
             if first_char == "完":
-                target_pattern = re.compile(fr"完[{second_char}][-–—]?{after_hyphen_int}")
+                target_pattern = re.compile(fr"完.?{second_char}[-–—]?{after_hyphen_int}")
                 st.write(target_pattern)
                 for bbox, text, prob in results:
                     cleaned = text.replace(" ", "")
