a4b60e52863e3fd0846dd89fba28e44eec8467bd
aghum229 <m229@aitech-inc.co.jp>
2025-08-18 17:14:31 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -48,7 +48,7 @@ else:
             if first_char == "完":
                 if os.path.basename(image_path) == "TanaMap20250815_1.png":
                     # image_search_flag = True
-                    target_pattern = re.compile(r"完[ABC][-–—]?(1[0-5]|[1-9])")
+                    target_pattern = re.compile(r"完[ABCD][-–—]?(1[0-5]|[1-9])")
                     for bbox, text, prob in results:
                         cleaned = text.replace(" ", "")
                         if target_pattern.search(cleaned):
@@ -57,6 +57,15 @@ else:
                             center_y = int((tl[1] + br[1]) / 2)
                             target_center = (center_x, center_y)
                             break
+                    image_with_circle = image_np.copy()
+                    if target_center:
+                        cv2.circle(image_with_circle, target_center, 50, (255, 0, 0), thickness=8)
+                        st.image(image_with_circle, caption=f"{target_text} を検出しました", use_container_width=True)
+                        st.success(f"座標: {target_center}")
+                        image_flag = True
+                        break
+                    else:
+                        None
                 else:
                     break
             elif (first_char == "E" and 31 <= after_hyphen <= 37) or (first_char == "G" and after_hyphen <= 18) or (first_char == "H" and after_hyphen <= 18) or (first_char == "R" and after_hyphen <= 19):
