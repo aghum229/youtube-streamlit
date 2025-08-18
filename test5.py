d12b1c221d92619490fc23c4df8fb276a0b2c9a8
aghum229 <m229@aitech-inc.co.jp>
2025-08-18 18:38:50 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -86,6 +86,7 @@ else:
             target_center = None
             if first_char == "完":
                 target_pattern = re.compile(r"完[{second_char}][-–—]?{after_hyphen_int}")
+                st.write(target_pattern)
                 for bbox, text, prob in results:
                     cleaned = text.replace(" ", "")
                     if target_pattern.search(cleaned):
