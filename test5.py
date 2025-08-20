e26d79f64d37718e756d2f455f1414fb3204041a
aghum229 <m229@aitech-inc.co.jp>
2025-08-20 12:52:26 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -107,10 +107,6 @@ else:
                         (tl, tr, br, bl) = bbox
                         center_x = int((tl[0] + br[0]) / 2)
                         center_y = int((tl[1] + br[1]) / 2)
-                        if second_char == "B" or second_char == "D":
-                            center_x += 10
-                        else:
-                            center_x -= 10
                         target_center = (center_x, center_y)
                         break
                     else:
