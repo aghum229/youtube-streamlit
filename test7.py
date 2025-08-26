4dcc0860a327b4929fe25c891fd8f91976f87ed7
aghum229 <m229@aitech-inc.co.jp>
2025-08-27 08:23:21 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -882,7 +882,7 @@ else:
                             st.rerun()
                     # button_key = "check_ok"
                     # st.session_state[button_key] = False
-                    if "check_ok" not in st.session_state:
+                    if st.session_state.production_order /= "" and "check_ok" not in st.session_state:
                         # if st.button("確認"):
                         message_text = f"""
                         #### 現在選択されている棚番 : {st.session_state.tanaban_select_temp}
