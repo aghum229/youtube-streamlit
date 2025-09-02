e168bf8cef5ed2036891c14f98d26b38c530e052
aghum229 <m229@aitech-inc.co.jp>
2025-09-02 18:54:52 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -752,7 +752,7 @@ else:
                     
                     records = data_catch_hinmoku(st.session_state.sf, manual_input_hinban)
                     if records:
-                        hinban_list = records["snps_um__ItemName__c"].tolist()  # zk履歴 AITC_ID18__c, snps_um__ItemName__c, AITC_PrintItemName__c
+                        hinban_list = [r["snps_um__ItemName__c"] for r in records]  # zk履歴 AITC_ID18__c, snps_um__ItemName__c, AITC_PrintItemName__c
                         # st.write(f"選択前棚番号: {tanaban_select}")
                         hinban_select = st.selectbox(
                             "品番を選んでください", hinban_list, key="hinban_select"
