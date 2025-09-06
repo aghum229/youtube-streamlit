67cc6ed4b1c9542fd07c8ab30bb5d532d79ceb9a
aghum229 <m229@aitech-inc.co.jp>
2025-09-06 14:48:22 +0900

    Update test5.py

--- test5.py
+++ test5.py
@@ -191,10 +191,10 @@ else:
                         center_y = int((tl[1] + br[1]) / 2)
                         target_center = (center_x, center_y)
                         break
-                    elif (text.strip() == "R-g" and target_text.strip() == "R-9")
+                    elif ((text.strip() == "R-g" and target_text.strip() == "R-9")
                         or (text.strip() == "6-5" and target_text.strip() == "G-5") 
                         or (text.strip() == "6-6" and target_text.strip() == "G-6") 
-                        or (text.strip() == "6-17" and target_text.strip() == "G-17"):
+                        or (text.strip() == "6-17" and target_text.strip() == "G-17")):
                         (tl, tr, br, bl) = bbox
                         center_x = int((tl[0] + br[0]) / 2)
                         center_y = int((tl[1] + br[1]) / 2)
