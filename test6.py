3b59f70de88b4dae34dd4f1e7a6aa7c0cf70f4ee
aghum229 <m229@aitech-inc.co.jp>
2025-09-10 19:09:17 +0900

    Update test6.py

--- test6.py
+++ test6.py
@@ -1009,9 +1009,10 @@ else:
                                 else:
                                     st.write("'item_id'　が存在しません。至急、システム担当者に連絡してください！")
                                     st.stop()
+                                selected_tanaban =　""
                                 # tanban_list = ["---"] + sorted(st.session_state.df_search_result.iloc[:, 0].dropna().unique())
                                 tanban_list = ["---"] + st.session_state.df_search_result.iloc[:, 0].dropna().tolist()
-                                selected_tanaban = st.selectbox("棚番を選択してください", tanban_list)
+                                selected_tanaban = st.selectbox("棚番を選択してください　(クリックするとリストが開きます)", tanban_list)
                                 # selected_tanaban = st.selectbox("棚番を選択してください　(クリックするとリストが開きます)", st.session_state.df_search_result["棚番"])
                                 st.session_state.tanaban_select_value = selected_tanaban
                                 if st.session_state.tanaban_select_value != "" and st.session_state.tanaban_select_value != "---":
