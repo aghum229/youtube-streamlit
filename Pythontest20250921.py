39b13e75c0a3a89ef2417bcaaeb0e5053e6f03e2
aghum229 <m229@aitech-inc.co.jp>
2025-10-01 02:52:31 +0900

    Update Pythontest20250921.py

--- Pythontest20250921.py
+++ Pythontest20250921.py
@@ -2049,7 +2049,14 @@ def zaiko_place():
                             process_order = 0
                             process_order_name = "-"
                             quantity = 0.0
-                            
+
+                        st.markdown("""
+                            <style>
+                            div.stButton > button {
+                                white-space: pre-line;
+                            }
+                            </style>
+                        """, unsafe_allow_html=True)
                         st.session_state.add_del_flag = 0  # 0:追加 1:削除 2:持出 3:持出解除 9:取消     
                         left, center1, center2, right = st.columns(4)
                         with left:
