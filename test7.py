d0dd29a5f6921a0ad613ef4c0d99f04139fcc650
aghum229 <m229@aitech-inc.co.jp>
2025-10-28 18:25:30 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import urllib.parse
 import streamlit.components.v1 as components
-import textwrap
 
 st.title("QRコード読み取り")
 
@@ -15,31 +14,32 @@ if qr_result:
         st.query_params.clear()
         st.rerun()
 else:
-    html_code = textwrap.dedent("""
-        <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
-        <div id="reader" style="width:300px;"></div>
-        <script>
-        function startScanner() {
-            const html5QrCode = new Html5Qrcode("reader");
-            html5QrCode.start(
-                { facingMode: "environment" },
-                { fps: 10, qrbox: 250 },
-                function(decodedText, decodedResult) {
-                    const baseUrl = window.location.origin + window.location.pathname;
-                    const newUrl = baseUrl + "?qr=" + encodeURIComponent(decodedText);
-                    window.location.href = newUrl;
-                },
-                function(errorMessage) {
-                    console.log("読み取りエラー:", errorMessage);
-                }
-            );
-        }
+    html_code = """
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <div id="reader" style="width:300px;"></div>
+    <script>
+      function onScanSuccess(decodedText, decodedResult) {
+        const baseUrl = window.location.origin + window.location.pathname;
+        const newUrl = baseUrl + "?qr=" + encodeURIComponent(decodedText);
+        window.location.href = newUrl;
+      }
 
-        if (!window.qrScannerInitialized) {
-            startScanner();
-            window.qrScannerInitialized = true;
-        }
-        </script>
-    """)
+      function startScanner() {
+        const html5QrCode = new Html5Qrcode("reader");
+        html5QrCode.start(
+          { facingMode: "environment" },
+          { fps: 10, qrbox: 250 },
+          onScanSuccess,
+          (errorMessage) => { console.log("読み取りエラー:", errorMessage); }
+        );
+      }
 
-    components.html(html_code, height=400, key="qr-reader")
+      if (!window.qrScannerInitialized) {
+        startScanner();
+        window.qrScannerInitialized = true;
+      }
+    </script>
+    """
+
+    components.html(html_code.strip(), height=400)
+
