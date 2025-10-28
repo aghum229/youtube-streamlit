94b3f2e6fe57b08011327e10e6b9fd0320d493e7
aghum229 <m229@aitech-inc.co.jp>
2025-10-28 17:42:44 +0900

    Refactor HTML code generation using textwrap

--- test7.py
+++ test7.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import urllib.parse
 import streamlit.components.v1 as components
+import textwrap
 
 st.title("QRコード読み取り")
 
@@ -14,31 +15,31 @@ if qr_result:
         st.query_params.clear()
         st.rerun()
 else:
-    html_code = """
-    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
-    <div id="reader" style="width:300px;"></div>
-    <script>
-      function startScanner() {
-        const html5QrCode = new Html5Qrcode("reader");
-        html5QrCode.start(
-          { facingMode: "environment" },
-          { fps: 10, qrbox: 250 },
-          function(decodedText, decodedResult) {
-            const baseUrl = window.location.origin + window.location.pathname;
-            const newUrl = baseUrl + "?qr=" + encodeURIComponent(decodedText);
-            window.location.href = newUrl;
-          },
-          function(errorMessage) {
-            console.log("読み取りエラー:", errorMessage);
-          }
-        );
-      }
+    html_code = textwrap.dedent("""
+        <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+        <div id="reader" style="width:300px;"></div>
+        <script>
+        function startScanner() {
+            const html5QrCode = new Html5Qrcode("reader");
+            html5QrCode.start(
+                { facingMode: "environment" },
+                { fps: 10, qrbox: 250 },
+                function(decodedText, decodedResult) {
+                    const baseUrl = window.location.origin + window.location.pathname;
+                    const newUrl = baseUrl + "?qr=" + encodeURIComponent(decodedText);
+                    window.location.href = newUrl;
+                },
+                function(errorMessage) {
+                    console.log("読み取りエラー:", errorMessage);
+                }
+            );
+        }
 
-      if (!window.qrScannerInitialized) {
-        startScanner();
-        window.qrScannerInitialized = true;
-      }
-    </script>
-    """
+        if (!window.qrScannerInitialized) {
+            startScanner();
+            window.qrScannerInitialized = true;
+        }
+        </script>
+    """)
 
     components.html(html_code, height=400, key="qr-reader")
